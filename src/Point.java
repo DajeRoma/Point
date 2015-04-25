4f7aebd9aae6305a0ba951ccf17139b915fc8257
DajeRoma <yiting.ju@gmail.com>
2015-04-24 17:24:54 -0700

    Revise library issue and add System.out.print

--- src/Point.java
+++ src/Point.java
@@ -1,9 +1,8 @@
-
 public class Point {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StdOut.print("Hello World， I Changed it again!!");	
+		System.out.print("Hello World， I Changed it again!!");	
 	}
 
 }
