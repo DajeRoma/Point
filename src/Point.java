394cb56bdf5594b053f170cbe6efd0a30ea8b167
Yiting Ju <yiting.ju@gmail.com>
2015-05-01 13:25:20 -0700

    Update Point.java
    Change to system.out.print(), which is more commonly used. Also test how github works.

--- src/Point.java
+++ src/Point.java
@@ -3,7 +3,7 @@ public class Point {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StdOut.print("Hello World， I Changed it again it second time!!");	
+		System.out.print("Hello World， I Changed it again it second time!!");	
 	}
 
 }
